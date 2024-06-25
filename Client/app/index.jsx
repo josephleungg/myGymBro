26740e9a4a10e8aafbfc093d5335a0eb0f571a42
Joseph Leung <leung.joseph@outlook.com>
2024-06-25 00:18:51 -0400

    feat(homepage): Added homepage logo and router to auth
    Buttons to continue to sign up and sign in
    App logo and slogan added

--- Client/app/index.jsx
+++ Client/app/index.jsx
@@ -1,14 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Image, Text, View } from 'react-native';
-import { Link } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Redirect, router } from 'expo-router';
+import images from '../helper/images.js'
+import CustomButton from '../components/custombutton.jsx'
 
 export default function App() {
+
   return (
-    <View className="flex-1 items-center justify-center bg-primary">
-      <Text className="text-xl color-gray-100 font-plight">myGymBro</Text>
-      <StatusBar style="auto" />
-      <Link href="/home" className="color-blue-300 font-plight">Home</Link>
-    </View>
+    
+    <SafeAreaView className="bg-primary h-full">
+        
+        {/* big div box holding all elements in one full screen */}
+        <View className="w-full items-center h-full px-4">
+
+          {/* App Logo and slogan */}
+          <View className="items-center">
+            <Text className='font-psemibold text-lg color-white mt-10'>my<Text className='color-secondary'>GymBro</Text></Text>
+            <Image 
+              source={images.transBGLogo} 
+              className='w-[128px] h-[128px]'
+              resizeMode='contain'
+            />
+            <Text className='font-pregular text-lg color-white text-center'>your pocket <Text className='color-secondary'>Gym Buddy</Text> </Text>
+          </View>
+
+          {/* sign in or sign up button */}
+          <View className="w-full h-full justify-center">
+            <CustomButton 
+              title="Continue with Email"
+              handlePress={() => router.push('/sign-in')}
+              containerStyles="w-full mt-7"
+            />
+          </View>
+
+        </View>
+
+        {/* statusbar customization */}
+        <StatusBar backgroundColor='#161622' style='light'/>
+
+    </SafeAreaView>
+
   );
 }
---
+++ Client/helper/images.js
@@ -0,0 +1,9 @@
+import transBGLogo from '../assets/logo/gymbro-trans-bg.png';
+import primaryBGLogo from '../assets/logo/gymbro-primary-bg.png';
+import blackBGLogo from '../assets/logo/gymbro-black-bg.png';
+
+export default {
+    transBGLogo,
+    primaryBGLogo,
+    blackBGLogo,
+};
