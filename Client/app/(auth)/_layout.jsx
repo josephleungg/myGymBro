182c40d2281a39afc0af5e3dc8b89efeaa29cec3
Joseph Leung <leung.joseph@outlook.com>
2024-06-25 00:44:39 -0400

    setting up auth

--- Client/app/(auth)/_layout.jsx
+++ Client/app/(auth)/_layout.jsx
@@ -1,10 +1,18 @@
 import { View, Text } from 'react-native'
+import { Stack } from 'expo-router'
+import { StatusBar } from 'expo-status-bar'
 import React from 'react'
 
 export default function AuthLayout() {
   return (
-    <View>
-      <Text>AuthLayout</Text>
-    </View>
-  )
+    <>
+      <Stack>
+        <Stack.Screen name="sign-in" options={{ headerShown: false }} />
+        <Stack.Screen name="sign-up" options={{ headerShown: false }} />
+      </Stack>
+
+      {/* statusbar customization */}
+      <StatusBar backgroundColor='#161622' style='light'/>
+    </>
+  );
 }
--- Client/app/(auth)/sign-in.jsx
+++ Client/app/(auth)/sign-in.jsx
@@ -1,10 +1,17 @@
-import { View, Text } from 'react-native'
+import { View, Text, SafeAreaView, ScrollView, Image } from 'react-native'
+import images from '../../helper/images.js'
 import React from 'react'
 
 export default function SignIn() {
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+
+    <SafeAreaView className="bg-primary h-full">
+
+        <View className="w-full justify-center min-h-[85vh] px-4 my-6">
+          <Text className="color-white">Sign In</Text>
+        </View>
+
+    </SafeAreaView>
+
   )
 }
