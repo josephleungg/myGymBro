a0e067aef7a2d9b9fa42a6ea2539a04afcbb52eb
Joseph Leung <leung.joseph@outlook.com>
2024-06-26 22:10:24 -0400

    created schema and collection for users

--- Server/index.js
+++ Server/index.js
@@ -1,5 +1,12 @@
 import express from 'express'
 import cors from 'cors'
+import mongoose from 'mongoose';
+import User from './models/users.model.js'
+
+// connecting to the database
+mongoose.connect('mongodb+srv://admin:k5H4dusEVAZXTy9O@mygymbro.dzpouaw.mongodb.net/myGymBro?retryWrites=true&w=majority&appName=myGymBro')
+  .then(() => console.log('Connected to db!'))
+  .catch((e) => {console.log(e)});
 
 // initialize express
 const app = express()
@@ -9,6 +16,18 @@ app.use(express.json())
 app.get('/', (req, res) => {
     res.status(200).json({'message': 'hello world'})
 })
+
+// signup API
+app.post('/signup', async (req, res) => {
+    
+    try {
+        const user = await User.create(req.body);
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+    
+});
 
 app.listen(5000, () => {
     console.log('server is running on port 5000')
---
+++ Server/models/users.model.js
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose';
+
+const UsersSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    properties: {
+        name: { type: String, default: '' },
+        age: { type: Number, default: 0 },
+        bio: { type: String, default: '', maxlength: 256},
+        sex: { type: String, default: '' },
+        weight: { type: Number, default: 0 },
+        height: { type: Number, default: 0 },
+        bodyFat: { type: Number, default: 0 },
+        daysAtGym: { type: Number, default: 0 },
+    },
+    dateCreated: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const User = mongoose.model('User', UsersSchema);
+
+export default User;
