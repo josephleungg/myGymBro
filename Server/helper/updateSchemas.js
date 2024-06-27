a3676d47dd434f555522f90b24c0fd3fcfa0b4bf
Joseph Leung <leung.joseph@outlook.com>
2024-06-27 14:21:02 -0400

    updated email in database schema

--- Server/helper/updateSchemas.js
+++ Server/helper/updateSchemas.js
@@ -4,15 +4,11 @@ import User from '../models/users.model.js'
 export default async function updateUsers() {
     const updateResult = await User.updateMany({}, {
         $set: {
-            "properties.name": "",
-            "properties.age": 0,
-            "properties.bio": "",
-            "properties.sex": "",
-            "properties.weight": 0,
-            "properties.height": 0,
-            "properties.bodyFat": 0,
-            "properties.daysAtGym": [],
-            "properties.progressPics": [],
+            email: {
+                type: String,
+                unique: true,
+                required: true
+            },
         }
     });
     console.log(updateResult); // This will log the result of the update operation
