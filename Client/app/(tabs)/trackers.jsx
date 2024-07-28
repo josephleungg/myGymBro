d09086b6101e0efbd996d4f3166185df9f100852
Joseph Leung <leung.joseph@outlook.com>
2024-07-28 14:16:50 -0400

    Tracker

--- Client/app/(tabs)/trackers.jsx
+++ Client/app/(tabs)/trackers.jsx
@@ -1,4 +1,4 @@
-import { View, Text, SafeAreaView } from 'react-native'
+import { View, Text, SafeAreaView, ScrollView, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { useGlobalContext } from '../../context/GlobalProvider.js';
 import { router } from 'expo-router'
@@ -9,11 +9,23 @@ export default function Trackers() {
 
   return (
     <SafeAreaView className="bg-primary h-full">
-      
-      <View className='items-center'>
-        <Text className='text-white'>trackers</Text>
-      </View>
-      
+      <ScrollView className="p-4">
+
+        <View className="items-center pb-8">
+          <Text className="text-white text-lg font-psemibold">Trackers</Text>
+        </View>
+
+        <View className="flex-row space-x-4">
+          <TouchableOpacity className="flex-1 bg-secondary py-5 rounded-3xl items-center" onPress={() => console.log("workouts")}>
+            <Text className="text-black font-psemibold text-lg">Workouts</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity className="flex-1 bg-secondary py-5 rounded-3xl items-center" onPress={() => console.log("meals")}>
+            <Text className="text-black font-psemibold text-lg">Meals</Text>
+          </TouchableOpacity>
+        </View>
+
+      </ScrollView>
     </SafeAreaView>
   )
 }
